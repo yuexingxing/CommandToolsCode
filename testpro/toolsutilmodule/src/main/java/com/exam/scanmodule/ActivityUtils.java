b1c507850ee8f015cb84b60d105b683e6b47f930
670176656@qq.com <=>
2020-01-19 17:09:24 +0800

    第一次上传

---
+++ testpro/toolsutilmodule/src/main/java/com/exam/scanmodule/ActivityUtils.java
@@ -0,0 +1,54 @@
+package com.exam.scanmodule;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
+import android.text.TextUtils;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.List;
+
+/**
+ * Function:
+ * Author Name: yuexingxing
+ * Date: 2020-01-17
+ * Copyright © 2006-2018 高顿网校, All Rights Reserved.
+ **/
+public class ActivityUtils {
+
+    /**
+     * 判断某个界面是否在前台
+     *
+     * @param activity 要判断的Activity
+     * @return 是否在前台显示
+     */
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    public static boolean isForeground(Activity activity) {
+        return isForeground(activity, activity.getClass().getName());//这里有个小细节，获取类的名称
+    }
+
+    /**
+     * 判断某个界面是否在前台
+     *
+     * @param context   Context
+     * @param className 界面的类名
+     * @return 是否在前台显示
+     */
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    public static boolean isForeground(Context context, String className) {
+        if (context == null || TextUtils.isEmpty(className))
+            return false;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> list = am.getRunningTasks(1);
+        if (list != null && list.size() > 0) {
+            ComponentName cpn = list.get(0).topActivity;
+            if (className.equals(cpn.getClassName()))
+                return true;
+        }
+        return false;
+    }
+}
+
---
+++ testpro/toolsutilmodule/src/main/java/com/exam/scanmodule/StringUtil.java
@@ -0,0 +1,39 @@
+package com.exam.scanmodule;
+
+import java.util.regex.Pattern;
+
+/**
+ * Function:
+ * Author Name: yuexingxing
+ * Date: 2020-01-17
+ * Copyright © 2006-2018 高顿网校, All Rights Reserved.
+ **/
+public class StringUtil {
+
+
+    /**
+     * 为空判断
+     * */
+    public static boolean isEmpty(String input) {
+        if (input == null || "".equals(input))
+            return true;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 手机号码判断
+     * */
+    public static boolean isPhoneNumber(String input) {// 判断手机号码是否规则
+        String regex = "(1[0-9][0-9]|15[0-9]|18[0-9])\\d{8}";
+        Pattern p = Pattern.compile(regex);
+        return p.matches(regex, input);//如果不是号码，则返回false，是号码则返回true
+    }
+}
